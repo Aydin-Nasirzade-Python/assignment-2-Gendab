040d224818ae52eaa350eaea96fdc270c57258a6
Gendab <165568329+Gendab@users.noreply.github.com>
2024-04-07 15:00:54 +0400

    Update Task_10.py

--- Task_10.py
+++ Task_10.py
@@ -3,8 +3,7 @@
 def main():
   x=float(input("Enter x: "))
   y=float(input("Enter y: "))
-  if y>=4-x**2 and y>=x**2 and x>=0 or x>=0 and y>=0 and y<=x**2 and y<=2-x or y<=4-x**2
-and y>=x**2 and x<=0 and y<=2-x or x<=0 and y<=x**2 and y>=2-x :
+  if y>=4-x**2 and y>=x**2 and x>=0 or x>=0 and y>=0 and y<=x**2 and y<=2-x or y<=4-x**2 and y>=x**2 and x<=0 and y<=2-x or x<=0 and y<=x**2 and y>=2-x :
       print('The point is in the shaded area')
   else: print('The point is not in the shaded area')
   pass
