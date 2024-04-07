4c9b856141c2f3fa1c507bb6904c91ce32017e2d
Gendab <165568329+Gendab@users.noreply.github.com>
2024-04-07 15:31:31 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -2,28 +2,29 @@
 
 def main():
   a=int(input("Enter the year [ex. 2021]: "))
-  if a<0: print("Invalid year!")
-  elif a==2000 or (a-2000)%12==0:
+  if a<0:
+      print("Invalid year!")
+  elif a%12==8:
       print(f"{a} is the year of the Dragon")
-  elif a==2001 or (il-2001)%12==0:
+  elif a%12==9:
       print(f"{a} is the year of the Snake")
-  elif a==2002 or (a-2002)%12==0:
+  elif a%12==10:
       print(f"{a} is the year of the Horse")
-  elif a==2003 or (a-2003)%12==0:
+  elif a%12==11:
       print(f"{a} is the year of the Sheep")
-  elif a==2004 or (a-2004)%12==0:
+  elif a%12==0:
       print(f"{a} is the year of the Monkey")
-  elif a==2005 or (a-2005)%12==0:
+  elif a%12==1:
       print(f"{a} is the year of the Rooster")
-  elif a==2006 or (a-2006)%12==0:
+  elif a%12==2:
       print(f"{a} is the year of the Dog")
-  elif a==2007 or (a-2007)%12==0:
+  elif a%12==3:
       print(f"{a} is the year of the Pig")
-  elif a==2008 or (a-2008)%12==0:
+  elif a%12==4
       print(f"{a} is the year of the Rat")
-  elif a==2009 or (a-2002)%12==0:
+  elif a%12==5:
       print(f"{a} is the year of the Ox")
-  elif a==2010 or (a-2010)%12==0:
+  elif a%12==6:
       print(f"{a} is the year of the Tiger")   
   else:
       print(f"{a} is the year of the Hare")
