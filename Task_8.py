9463558c4cb0c6c5ca098f05ec46d13cfabb72db
Gendab <165568329+Gendab@users.noreply.github.com>
2024-04-07 15:05:37 +0400

    Update Task_8.py

--- Task_8.py
+++ Task_8.py
@@ -6,8 +6,6 @@ def main():
   if y<x**2 and x>0 and y>0 and y<2-x or y<x**2 and x<0 and y>0 y>2-x:
       print('The point is in the shaded area')
   else: print('The point is not in the shaded area')
-
-
   pass
 
 if __name__ == "__main__":
