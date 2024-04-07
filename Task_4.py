6ed2ad5311a0b4b4e3aed2763551a6ada010b6fa
Gendab <165568329+Gendab@users.noreply.github.com>
2024-04-07 13:09:44 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,9 +1,33 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  il=int(input("Enter the year [ex. 2021]: "))
+  if il==2000 or (il-2000)%12==0:
+      print(f"{il} is the year of the Dragon")
+  elif il==2001 or (il-2001)%12==0:
+      print(f"{il} is the year of the Snake")
+  elif il==2002 or (il-2002)%12==0:
+      print(f"{il} is the year of the Horse")
+  elif il==2003 or (il-2003)%12==0:
+      print(f"{il} is the year of the Sheep")
+  elif il==2004 or (il-2004)%12==0:
+      print(f"{il} is the year of the Monkey")
+  elif il==2005 or (il-2005)%12==0:
+      print(f"{il} is the year of the Rooster")
+  elif il==2006 or (il-2006)%12==0:
+      print(f"{il} is the year of the Dog")
+  elif il==2007 or (il-2007)%12==0:
+      print(f"{il} is the year of the Pig")
+  elif il==2008 or (il-2008)%12==0:
+      print(f"{il} is the year of the Rat")
+  elif il==2009 or (il-2002)%12==0:
+      print(f"{il} is the year of the Ox")
+  elif il==2010 or (il-2010)%12==0:
+      print(f"{il} is the year of the Tiger")
+  elif il<0:
+      print("Invalid year!")    
+  else:
+      print(f"{il} is the year of the Hare")
   pass
 
 if __name__ == "__main__":
