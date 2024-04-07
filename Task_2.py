2277dbe980b6e509baad4b440c34502fde6e2f81
Gendab <165568329+Gendab@users.noreply.github.com>
2024-04-07 11:49:23 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,9 +1,16 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  ay=input("Enter name of the month [ex. June]: ")
+  gun=input("Enter the day [ex. 5]: ")
+  if ay=="march" and gun=="20":
+      print("March 20 is in Spring")
+  elif ay=="June" and gun=="21":
+      print("June 21 is in Summer")
+  elif ay=="September" and gun=="22":
+      print("September 22 is in Fall")
+  elif ay=="December" and gun=="21":
+      print("December 21 is in Winter")
   pass
 
 if __name__ == "__main__":
