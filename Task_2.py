1f5bbd5300259e9b3e85a1fc503225eb2e6882b6
Gendab <165568329+Gendab@users.noreply.github.com>
2024-04-07 13:21:25 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,16 +1,19 @@
 #import libraries here
 
 def main():
+  a="AprilMay"
+  b="JulyAugust"
+  c="OctoberNovember"
   ay=input("Enter name of the month [ex. June]: ")
-  gun=input("Enter the day [ex. 5]: ")
-  if ay=="march" and gun=="20":
-      print("March 20 is in Spring")
-  elif ay=="June" and gun=="21":
-      print("June 21 is in Summer")
-  elif ay=="September" and gun=="22":
-      print("September 22 is in Fall")
-  elif ay=="December" and gun=="21":
-      print("December 21 is in Winter")
+  gun=int(input("Enter the day [ex. 5]: "))
+  if ay=="march" and gun>=20 or ay=="June" and gun<21 or ay in a:
+      print(f"{ay} {gun} is in Spring")
+  elif ay=="June" and gun>=21 or ay=="September" and gun<22 or ay in b:
+      print(f"{ay} {gun} is in Summer")
+  elif ay=="September" and gun>=22 or ay=="December" and gun<21 or ay in c:
+      print(f"{ay} {gun} is in Fall")
+  else:
+      print(f"{ay} {gun} is in Winter")
   pass
 
 if __name__ == "__main__":
